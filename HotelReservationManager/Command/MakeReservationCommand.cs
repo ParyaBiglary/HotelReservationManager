5ad99b839d6c38d2ade835da02d0817516ceaf23
parya biglary <paryabiglary@gmail.com>
2023-09-06 16:43:29 +0330

    Improved Submit Button

--- HotelReservationManager/Command/MakeReservationCommand.cs
+++ HotelReservationManager/Command/MakeReservationCommand.cs
@@ -6,6 +6,7 @@ namespace HotelReservationManager.Command
 {
     internal class MakeReservationCommand : CommandBase
     {
+
         #region Fields
 
         private readonly MakeReservationViewModel _makeReservationViewModel;
@@ -17,11 +18,19 @@ namespace HotelReservationManager.Command
         public MakeReservationCommand(MakeReservationViewModel makeReservationViewModel)
         {
             _makeReservationViewModel = makeReservationViewModel;
+            _makeReservationViewModel.PropertyChanged += _makeReservationViewModel_PropertyChanged;
         }
 
         #endregion Constructors
 
         #region Methods
+
+        public override bool CanExecute(object parameter)
+        {
+            return !string.IsNullOrEmpty(_makeReservationViewModel.UserName) && _makeReservationViewModel.FloorNumber > 0 &&
+                _makeReservationViewModel.RoomNumber > 0 &&
+                base.CanExecute(parameter);
+        }
 
         public override void Execute(object parameter)
         {
@@ -35,6 +44,17 @@ namespace HotelReservationManager.Command
             catch (ReservationConflictException)
             {
                 MessageBox.Show("This Room is already taken,SORRY!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void _makeReservationViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals(nameof(MakeReservationViewModel.UserName))
+                || e.PropertyName.Equals(nameof(MakeReservationViewModel.FloorNumber))
+                || e.PropertyName.Equals(nameof(MakeReservationViewModel.RoomNumber))
+                )
+            {
+                OnCanExecuteChanged();
             }
         }
 
